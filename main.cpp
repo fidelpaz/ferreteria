ef7fa514af8e0c5b108988af0c07a8017c84c764
fidelpaz <98630405+fidelpaz@users.noreply.github.com>
2022-08-03 20:29:08 -0600

    fidelholamundo

--- main.cpp
+++ main.cpp
@@ -12,6 +12,7 @@ using namespace std;
 int main() {
   cout << "Hola mundo" << endl;
   cout<<"Hola mundo 2"<<endl;
+  cout<<"Hola mundo 3"<<endl;
   cout<<"Todo bien?, Todo correcto?"<<endl;
   int opcion = 0;
   do {
